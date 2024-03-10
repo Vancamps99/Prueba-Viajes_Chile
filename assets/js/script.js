bdb56abcca178ea9cc1a558e588822512f0a8d75
Alberto Neculhueque <aang1999cl@gmail.com>
2024-03-09 23:16:38 -0300

    agregando script de informacion enviada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,30 @@
-
 //smooth scroll navbar //
-$(document).ready(function(){
-    // Función para desplazamiento suave hacia secciones
-    $('a.nav-link').on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 200, function(){
+$(document).ready(function () {
+  // Función para desplazamiento suave hacia secciones
+  $("a.nav-link").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        200,
+        function () {
           window.location.hash = hash;
-        });
-      }
-    });
+        }
+      );
+    }
   });
+});
+
+// Función para mostrar el mensaje de éxito
+function mostrarMensajeEnviado(event) {
+  event.preventDefault(); // Evitar el envío del formulario
+  var mensajeEnviado = document.getElementById("mensaje-enviado");
+  mensajeEnviado.style.display = "block";
+}
+
+// Agregar evento de clic al botón de enviar
+var botonEnviar = document.querySelector("button[type='submit']");
+botonEnviar.addEventListener("click", mostrarMensajeEnviado);
